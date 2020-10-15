0453f24bce9a162f3a1b5a70f59f24b185bd54b9
Federico Miranda <fmiranda@emergencias.com.ar>
2020-10-15 15:29:10 -0300

    get questions associated with selected symptoms from firestore. Work in progress

--- src/components/OnlineDoctor/Questions/index.js
+++ src/components/OnlineDoctor/Questions/index.js
@@ -13,37 +13,40 @@ import BackButton from '../../GeneralComponents/Backbutton'
 import '../../../styles/questions.scss';
 import '../../../styles/onlinedoctor/QuestionsModal.scss';
 import isIos from '../../Utils/isIos';
+import DB from '../../../config/DBConnection';
 
-const covidActivators = [
-	'Tos',
-	'Fiebre',
-	'Dolor de garganta',
-	'Sospecha de covid',
-	'Dolor de cabeza',
-	'Vómitos',
-	'Diarrea',
-	'mialgias'
-];
-const htaActivators = ['Hipertensión', 'Dolor de pecho'];
-const cameraActivators = [
-	'Problemas en la piel',
-	'salpullido',
-	'sarpullido',
-	'roncha',
-	'ampolla',
-	'lunar',
-	'quemadura',
-	'erupción',
-	'irritación',
-	'orzuelo',
-	'picazón',
-	'hinchazón',
-	'manchas',
-	'sarna',
-	'verruga',
-	'grano',
-	'piel'
-];
+const db = DB.firestore();
+
+// const covidActivators = [
+// 	'Tos',
+// 	'Fiebre',
+// 	'Dolor de garganta',
+// 	'Sospecha de covid',
+// 	'Dolor de cabeza',
+// 	'Vómitos',
+// 	'Diarrea',
+// 	'mialgias'
+// ];
+// const htaActivators = ['Hipertensión', 'Dolor de pecho'];
+// const cameraActivators = [
+// 	'Problemas en la piel',
+// 	'salpullido',
+// 	'sarpullido',
+// 	'roncha',
+// 	'ampolla',
+// 	'lunar',
+// 	'quemadura',
+// 	'erupción',
+// 	'irritación',
+// 	'orzuelo',
+// 	'picazón',
+// 	'hinchazón',
+// 	'manchas',
+// 	'sarna',
+// 	'verruga',
+// 	'grano',
+// 	'piel'
+// ];
 
 const Questions = () => {
 	const dispatch = useDispatch();
@@ -97,212 +100,240 @@ const Questions = () => {
 		dispatch({ type: 'SET_CURRENT_QUESTION', payload: currentQuestion });
 	}, [assessment.selectedQuestions, dispatch, i, j]);
 
-	useEffect(() => {
-		try {
-			const isIos = () => {
-				const userAgent = window.navigator.userAgent.toLowerCase();
-				return /iphone|ipad|ipod/.test(userAgent);
-			};
-			if (!isIos) {
-				navigator.permissions
-					.query({ name: 'geolocation' })
-					.then(function(result) {
-						if (result.state === 'granted') {
-							navigator.geolocation.getCurrentPosition(currentPos, watchError, posOptions);
-						} else if (result.state === 'prompt') {
-							navigator.geolocation.getCurrentPosition(currentPos, watchError, posOptions);
-						}
-						// result.state === 'denied'
-					})
-					.catch((err) => {
-						setCoordinates({ lat: '', lng: '' });
-					});
-			} else {
-				navigator.geolocation.getCurrentPosition(currentPos, watchError, posOptions);
-			}
-		} catch (err) {
-			setCoordinates({ lat: '', lng: '' });
-		}
-	}, []);
+	// useEffect(() => {
+	// 	try {
+	// 		const isIos = () => {
+	// 			const userAgent = window.navigator.userAgent.toLowerCase();
+	// 			return /iphone|ipad|ipod/.test(userAgent);
+	// 		};
+	// 		if (!isIos) {
+	// 			navigator.permissions
+	// 				.query({ name: 'geolocation' })
+	// 				.then(function(result) {
+	// 					if (result.state === 'granted') {
+	// 						navigator.geolocation.getCurrentPosition(currentPos, watchError, posOptions);
+	// 					} else if (result.state === 'prompt') {
+	// 						navigator.geolocation.getCurrentPosition(currentPos, watchError, posOptions);
+	// 					}
+	// 					// result.state === 'denied'
+	// 				})
+	// 				.catch((err) => {
+	// 					setCoordinates({ lat: '', lng: '' });
+	// 				});
+	// 		} else {
+	// 			navigator.geolocation.getCurrentPosition(currentPos, watchError, posOptions);
+	// 		}
+	// 	} catch (err) {
+	// 		setCoordinates({ lat: '', lng: '' });
+	// 	}
+	// }, []);
 
-	useEffect(() => {
-		const covidActive = covidActivators.some((activator) => assessment.selectedSymptoms.includes(activator));
-		const htaActive = htaActivators.some((activator) => assessment.selectedSymptoms.includes(activator));
-		const cameraActive = cameraActivators.some(
-			(activator) =>
-				assessment.selectedSymptoms.includes(activator) || assessment.selectedOtherSymptoms.includes(activator)
-		);
+	// useEffect(() => {
+	// 	const covidActive = covidActivators.some((activator) => assessment.selectedSymptoms.includes(activator));
+	// 	const htaActive = htaActivators.some((activator) => assessment.selectedSymptoms.includes(activator));
+	// 	const cameraActive = cameraActivators.some(
+	// 		(activator) =>
+	// 			assessment.selectedSymptoms.includes(activator) || assessment.selectedOtherSymptoms.includes(activator)
+	// 	);
 
-		if (covidActive) {
-			setModals({ ...modals, questions: true });
-			setAlerta('COVID');
-		}
-		if (htaActive && !covidActive) {
-			// setHtaModal(true);
-			setModals({ ...modals, htaModal: true });
-			setAlerta('HTA');
-		}
-		if (cameraActive) {
-			setModals({ ...modals, cameraModal: true });
-		}
-	}, []);
+	// 	if (covidActive) {
+	// 		setModals({ ...modals, questions: true });
+	// 		setAlerta('COVID');
+	// 	}
+	// 	if (htaActive && !covidActive) {
+	// 		// setHtaModal(true);
+	// 		setModals({ ...modals, htaModal: true });
+	// 		setAlerta('HTA');
+	// 	}
+	// 	if (cameraActive) {
+	// 		setModals({ ...modals, cameraModal: true });
+	// 	}
+	// }, []);
 
-	const watchError = () => console.log('Hubo un error al rastrear la posición');
+	// const watchError = () => console.log('Hubo un error al rastrear la posición');
 
-	const currentPos = ({ coords }) =>
-		setCoordinates({ lat: coords.latitude.toString() || '', lng: coords.longitude.toString() || '' });
+	// const currentPos = ({ coords }) =>
+	// 	setCoordinates({ lat: coords.latitude.toString() || '', lng: coords.longitude.toString() || '' });
+	
 	// Effect that get all selected questions for the patient's symptoms and save them to store
 	const questionsForEachSymptom = () => {
 		let selectedQuestions = [];
-		assessment.selectedSymptoms.forEach((symptom) => {
-			let filterQuestions = questions.find((t) => {
-				if (t.symptom === symptom && t.questions.length > 0) return t;
-			});
-			if (!!filterQuestions) selectedQuestions.push(filterQuestions.questions);
-		});
+		db.collection('/parametros/userapp/assessment').where('triggers', 'array-contains-any', assessment.selectedSymptoms).get()
+		.then(query => {
+			query.forEach(doc => {
+				const data = doc.data();
+				selectedQuestions.push({
+					id: doc.id,
+					question: data.question,
+					answers: data.answer
+				});
+
+				data.features.map(question => {
+					db.collection('parametros').doc('userapp').collection('assessment').doc(question).get()
+					.then(doc => {
+						if(doc.exists) {
+							const docData = doc.data();
+							selectedQuestions.push({
+								id: doc.id,
+								question: docData.question,
+								answers: docData.answer
+							});
+						}
+					})
+				})
+			})
+		})
+
+
+		// assessment.selectedSymptoms.forEach((symptom) => {
+		// 	let filterQuestions = questions.find((t) => {
+		// 		if (t.symptom === symptom && t.questions.length > 0) return t;
+		// 	});
+		// 	if (!!filterQuestions) selectedQuestions.push(filterQuestions.questions);
+		// });
 		dispatch({ type: 'SET_SELECTED_QUESTIONS', payload: selectedQuestions });
 	};
 
-	const habitatResponseCondition = (responses) => {
-		console.log(ubicacion)
-		if (ubicacion === 'provincia') {
-			if (
-				(responses.includes('contacto estrecho') && !responses.includes('personal esencial'))
-				|| responses.includes('anosmia/disgeusia')
-				|| responses.includes('convive con caso confirmado')
-				|| (responses.includes('contacto estrecho') && covidActivators.some((activator) => responses.includes(activator.toLocaleLowerCase())))
-				) {
-				setResponseIA({
-					...responseIA,
-					destino_final: 'En domicilio con instrucciones',
-					diagnostico: 'INESP   Confirmado COVID19 x epidemiol',
-				});
-				setModals({ ...modals, fever: false, habitat: false, termsAndConditions: true });
-			} else if (responses.includes('posee habitacion individual') && responses.length === 1) {
-				setResponseIA({ ...responseIA, destino_final: 'En domicilio con monitoreo' });
-				setModals({ ...modals, fever: false, habitat: false, termsAndConditions: true });
-			} else {
-				setModals({ ...modals, fever: false, habitat: false });
-			}
-		} else if (ubicacion === 'capital') {
-			if ((responses.includes('contacto estrecho') && !responses.includes('personal esencial'))
-			|| (responses.includes('contacto estrecho') && covidActivators.some((activator) => responses.includes(activator.toLocaleLowerCase())
-			|| responses.includes('anosmia/disgeusia')
-			|| responses.includes('convive con caso confirmado')))) {
-				setResponseIA({
-					...responseIA,
-					destino_final: 'En domicilio con instrucciones',
-					diagnostico: 'INESP   Confirmado COVID19 x epidemiol',
-				});
-				setModals({ ...modals, fever: false, habitat: false, termsAndConditions: true });
-			} else if (responses.includes('posee habitacion individual') && responses.length === 1) {
-				setResponseIA({ ...responseIA, destino_final: 'En domicilio con monitoreo' });
-				setModals({ ...modals, fever: false, habitat: false, termsAndConditions: true });
-			} else if (
-				responses.length > 0 &&
-				!responses.includes('internacion reciente - no lleva control de sus antecedentes') &&
-				!responses.includes('contacto estrecho')
-			) {
-				setResponseIA({ ...responseIA, destino_final: 'En domicilio con monitoreo' });
-				setModals({ ...modals, fever: false, habitat: false, termsAndConditions: true });
-			} else if (responses.length > 0) {
-				setResponseIA({ ...responseIA, destino_final: 'Traslado protocolo pandemia' });
-				setModals({ ...modals, fever: false, habitat: false });
-			} else {
-				setModals({ ...modals, fever: false, habitat: false });
-			}
-		} else {
-			setModals({ ...modals, fever: false, habitat: false });
-		}
-	};
+	// const habitatResponseCondition = (responses) => {
+	// 	console.log(ubicacion)
+	// 	if (ubicacion === 'provincia') {
+	// 		if (
+	// 			(responses.includes('contacto estrecho') && !responses.includes('personal esencial'))
+	// 			|| responses.includes('anosmia/disgeusia')
+	// 			|| responses.includes('convive con caso confirmado')
+	// 			|| (responses.includes('contacto estrecho') && covidActivators.some((activator) => responses.includes(activator.toLocaleLowerCase())))
+	// 			) {
+	// 			setResponseIA({
+	// 				...responseIA,
+	// 				destino_final: 'En domicilio con instrucciones',
+	// 				diagnostico: 'INESP   Confirmado COVID19 x epidemiol',
+	// 			});
+	// 			setModals({ ...modals, fever: false, habitat: false, termsAndConditions: true });
+	// 		} else if (responses.includes('posee habitacion individual') && responses.length === 1) {
+	// 			setResponseIA({ ...responseIA, destino_final: 'En domicilio con monitoreo' });
+	// 			setModals({ ...modals, fever: false, habitat: false, termsAndConditions: true });
+	// 		} else {
+	// 			setModals({ ...modals, fever: false, habitat: false });
+	// 		}
+	// 	} else if (ubicacion === 'capital') {
+	// 		if ((responses.includes('contacto estrecho') && !responses.includes('personal esencial'))
+	// 		|| (responses.includes('contacto estrecho') && covidActivators.some((activator) => responses.includes(activator.toLocaleLowerCase())
+	// 		|| responses.includes('anosmia/disgeusia')
+	// 		|| responses.includes('convive con caso confirmado')))) {
+	// 			setResponseIA({
+	// 				...responseIA,
+	// 				destino_final: 'En domicilio con instrucciones',
+	// 				diagnostico: 'INESP   Confirmado COVID19 x epidemiol',
+	// 			});
+	// 			setModals({ ...modals, fever: false, habitat: false, termsAndConditions: true });
+	// 		} else if (responses.includes('posee habitacion individual') && responses.length === 1) {
+	// 			setResponseIA({ ...responseIA, destino_final: 'En domicilio con monitoreo' });
+	// 			setModals({ ...modals, fever: false, habitat: false, termsAndConditions: true });
+	// 		} else if (
+	// 			responses.length > 0 &&
+	// 			!responses.includes('internacion reciente - no lleva control de sus antecedentes') &&
+	// 			!responses.includes('contacto estrecho')
+	// 		) {
+	// 			setResponseIA({ ...responseIA, destino_final: 'En domicilio con monitoreo' });
+	// 			setModals({ ...modals, fever: false, habitat: false, termsAndConditions: true });
+	// 		} else if (responses.length > 0) {
+	// 			setResponseIA({ ...responseIA, destino_final: 'Traslado protocolo pandemia' });
+	// 			setModals({ ...modals, fever: false, habitat: false });
+	// 		} else {
+	// 			setModals({ ...modals, fever: false, habitat: false });
+	// 		}
+	// 	} else {
+	// 		setModals({ ...modals, fever: false, habitat: false });
+	// 	}
+	// };
 
-	const switchModalContent = ({ fever, questions, habitat, termsAndConditions, htaModal, cameraModal }) => {
-		if (questions) {
-			return (
-				<Modal title='COVID-19' hideCloseButton>
-					<CovidModal
-						setResponseIA={(res) => {
-							setResponseIA(res);
-							const covidActive = covidActivators.some((activator) =>
-								assessment.selectedSymptoms.includes(activator)
-							);
-							if (
-								covidActive &&
-								res.diagnostico === 'INESP   Sospecha COVID19' &&
-								res.destino_final !== 'Evaluación en rojo' &&
-								res.destino_final !== 'Evaluación en amarillo'
-							) {
-								if (res.epicrisis.includes('fiebre')) {
-									setModals({ ...modals, questions: false, fever: true });
-								} else {
-									setModals({ ...modals, questions: false, habitat: true });
-								}
-							} else {
-								setModals({ ...modals, questions: false });
-							}
-						}}
-					/>
-				</Modal>
-			);
-		}
+	// const switchModalContent = ({ fever, questions, habitat, termsAndConditions, htaModal, cameraModal }) => {
+	// 	if (questions) {
+	// 		return (
+	// 			<Modal title='COVID-19' hideCloseButton>
+	// 				<CovidModal
+	// 					setResponseIA={(res) => {
+	// 						setResponseIA(res);
+	// 						const covidActive = covidActivators.some((activator) =>
+	// 							assessment.selectedSymptoms.includes(activator)
+	// 						);
+	// 						if (
+	// 							covidActive &&
+	// 							res.diagnostico === 'INESP   Sospecha COVID19' &&
+	// 							res.destino_final !== 'Evaluación en rojo' &&
+	// 							res.destino_final !== 'Evaluación en amarillo'
+	// 						) {
+	// 							if (res.epicrisis.includes('fiebre')) {
+	// 								setModals({ ...modals, questions: false, fever: true });
+	// 							} else {
+	// 								setModals({ ...modals, questions: false, habitat: true });
+	// 							}
+	// 						} else {
+	// 							setModals({ ...modals, questions: false });
+	// 						}
+	// 					}}
+	// 				/>
+	// 			</Modal>
+	// 		);
+	// 	}
 
-		if (fever && !isIos()) {
-			return (
-				<Modal title='Muestra médica' hideCloseButton>
-					<VideoInput
-						isModal={true}
-						finalAction={() => setModals({ ...modals, habitat: true, fever: false })}
-					/>
-				</Modal>
-			);
-		}
+	// 	if (fever && !isIos()) {
+	// 		return (
+	// 			<Modal title='Muestra médica' hideCloseButton>
+	// 				<VideoInput
+	// 					isModal={true}
+	// 					finalAction={() => setModals({ ...modals, habitat: true, fever: false })}
+	// 				/>
+	// 			</Modal>
+	// 		);
+	// 	}
 
-		if (habitat || (fever && isIos())) {
-			return (
-				<Modal title='Preguntas de hábitat' hideCloseButton>
-					<HabitatOrTerms
-						formHandler={() => habitatResponseCondition(responses)}
-						typeComponent='habitat'
-						ubicacion={ubicacion}
-						setUbicacion={setUbicacion}
-						mode='button'
-					/>
-				</Modal>
-			);
-		}
+	// 	if (habitat || (fever && isIos())) {
+	// 		return (
+	// 			<Modal title='Preguntas de hábitat' hideCloseButton>
+	// 				<HabitatOrTerms
+	// 					formHandler={() => habitatResponseCondition(responses)}
+	// 					typeComponent='habitat'
+	// 					ubicacion={ubicacion}
+	// 					setUbicacion={setUbicacion}
+	// 					mode='button'
+	// 				/>
+	// 			</Modal>
+	// 		);
+	// 	}
 
-		if (termsAndConditions) {
-			return (
-				<Modal title='Declaración jurada' hideCloseButton>
-					<HabitatOrTerms
-						formHandler={() => setModals({ ...modals, termsAndConditions: false })}
-						typeComponent='terminos'
-						ubicacion={ubicacion}
-						activateDefault={true}
-					/>
-				</Modal>
-			);
-		}
+	// 	if (termsAndConditions) {
+	// 		return (
+	// 			<Modal title='Declaración jurada' hideCloseButton>
+	// 				<HabitatOrTerms
+	// 					formHandler={() => setModals({ ...modals, termsAndConditions: false })}
+	// 					typeComponent='terminos'
+	// 					ubicacion={ubicacion}
+	// 					activateDefault={true}
+	// 				/>
+	// 			</Modal>
+	// 		);
+	// 	}
 
-		if (htaModal) {
-			return (
-				<Modal title='Hipertensión' hideCloseButton>
-					<HtaModal
-						unsetModal={() => setModals({ ...modals, htaModal: false })}
-						setResponseIA={setResponseIA}
-					/>
-				</Modal>
-			);
-		}
+	// 	if (htaModal) {
+	// 		return (
+	// 			<Modal title='Hipertensión' hideCloseButton>
+	// 				<HtaModal
+	// 					unsetModal={() => setModals({ ...modals, htaModal: false })}
+	// 					setResponseIA={setResponseIA}
+	// 				/>
+	// 			</Modal>
+	// 		);
+	// 	}
 
-		if (cameraModal) {
-			return (
-				<Modal title='Fotos' hideCloseButton>
-					<CameraInput modal={true} finalAction={() => setModals({ ...modals, cameraModal: false })} />
-				</Modal>
-			);
-		}
-	};
+	// 	if (cameraModal) {
+	// 		return (
+	// 			<Modal title='Fotos' hideCloseButton>
+	// 				<CameraInput modal={true} finalAction={() => setModals({ ...modals, cameraModal: false })} />
+	// 			</Modal>
+	// 		);
+	// 	}
+	// };
 
 	return (
 		<>
@@ -313,9 +344,9 @@ const Questions = () => {
 				<div className='assessment-text mt-4 mb-4'>
 					{assessment.currentQuestion && assessment.currentQuestion.title}
 				</div>
-				{switchModalContent(modals)}
+				{/* {switchModalContent(modals)} */}
 				<ContainerAssessmentAppointment {...propsContainerAssessmentAppointment} />
-			</div>
+			</div>￼
 		</>
 	);
 };
