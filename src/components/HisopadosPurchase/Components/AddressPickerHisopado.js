8526c5584190b32047424eea2de521f190e0de54
santiago-rodriguezs <srodriguez@emergencias.com.ar>
2020-10-29 10:30:47 -0300

    vars scss

--- src/components/HisopadosPurchase/Components/AddressPickerHisopado.js
+++ src/components/HisopadosPurchase/Components/AddressPickerHisopado.js
@@ -3,17 +3,17 @@ import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from "react-redux"
 import DeliverySelectDesiny from "../../DeliveryService/DeliverySelectDestiny"
 import { handleAddressValidForHisopado } from "../../../store/actions/deliveryActions"
+import { is_distance_valid_for_hisopado }from "../../../config/endpoints"
 import axios from 'axios'
 
 export default function AddressPickerHisopado({ finalAction }) {
     const dispatch = useDispatch();
-    const { addressLatLongHisopado } = useSelector(state => state.deliveryService)
+    const { addressLatLongHisopado } = useSelector(state => state.deliveryService);
 
     useEffect(() => {
         //CHEQUEAR QUE LA DIRECC CUMPLA CON EL RADIO DE COVERTURA
-        const distanceEndpoint = `http://localhost:8080/delivery/isDistanceValidForHisopado`
         console.log(addressLatLongHisopado)
-        axios.post(distanceEndpoint, {lat: addressLatLongHisopado.lat, lng: addressLatLongHisopado.lng})
+        axios.post(is_distance_valid_for_hisopado, {lat: addressLatLongHisopado.lat, lng: addressLatLongHisopado.lng})
         .then(content => {
             const isValid = content.data.isValid
             console.log(isValid)
