5a77c161f6ad07e2931d1ddaa19398285e030aa0
Gustavo Rojas <grojas@emergencias.com.ar>
2020-10-30 18:14:19 -0300

    k

--- src/components/Utils/mapsApiHandlers.js
+++ src/components/Utils/mapsApiHandlers.js
@@ -12,3 +12,32 @@ export const errorHandler = (err) =>
 		icon: 'warning',
 		dangerMode: true,
 	});
+
+
+export const calculateDistance = ({origin, destiny}) => {
+	return new Promise((resolve, reject) => {
+		const originPoint = {
+			lat: origin?.lat,
+			lng: origin?.lng
+		};
+		const destinyPoint = {
+			lat: destiny?.lat,
+			lng: destiny?.lng
+		};
+		const DirectionsService = new window.google.maps.DirectionsService();
+		DirectionsService.route({
+				origin: originPoint,
+				destination: destinyPoint,
+				travelMode: window.google.maps.TravelMode.DRIVING
+		}, (result, status) => {
+				if (status === window.google.maps.DirectionsStatus.OK) {
+					console.log(status);
+					resolve(result);
+				} else {
+					console.error(`error fetching directions ${result}`);
+					reject(result);
+				}
+		});
+	})
+}
+	
