e1825e3e1ec2856516d073fa99618bdf28b0b562
Federico Miranda <fmiranda@emergencias.com.ar>
2020-10-30 16:22:00 -0300

    home with new styles

--- src/components/DeliveryService/BuyButton/index.jsx
+++ src/components/DeliveryService/BuyButton/index.jsx
@@ -4,10 +4,12 @@ import {useSelector} from 'react-redux';
 import '../../../styles/hisopado/delivery.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import HomeHisopado from '../../../assets/img/home-hisopado.png';
 
 const BuyHisopado = () => {
     const history = useHistory()
     const patient = useSelector((state) => state.queries.patient)
+    const price = useSelector((state) => state.deliveryService.params.price);
 
     const buyHisopado = () => {
 		window.gtag('event', 'select_item', {
@@ -17,8 +19,17 @@ const BuyHisopado = () => {
     }
     
     return <section className="hisopado__container" onClick={() => buyHisopado()}>
-        <h2 className="hisopado__title">¡Comprá tu hisopado a domicilio!</h2>
-        <button className="hisopado__btn">Conocer más <FontAwesomeIcon icon={faArrowRight} /></button>
+        <img src={HomeHisopado} className="hisopado__img" alt="¡Hisopate hoy!"/>
+        <div className="hisopado__content">
+            <div className="hisopado__info">
+                <h2 className="hisopado__title">¡Hisopate hoy!</h2>
+                <p className="hisopado__text">Hacete tu testeo a domicilio.</p>
+                <button className="hisopado__btn">Conocer más <FontAwesomeIcon icon={faArrowRight} /></button>
+            </div>
+            <div className="hisopado__price">
+                <p>A sólo <span>${price}</span></p>
+            </div>
+        </div>
     </section>
 }
 
