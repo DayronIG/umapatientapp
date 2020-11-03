126af45cc8158654efeb7a13b3a4c0a183a3946a
Federico Miranda <fmiranda@emergencias.com.ar>
2020-11-03 12:48:09 -0300

    new notService styles

--- src/components/DeliveryService/NotService.js
+++ src/components/DeliveryService/NotService.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import noservice from "../../assets/icons/underConstruction.png";
-import FooterBtn from "../GeneralComponents/FooterBtn";
+import { useSelector } from 'react-redux';
+import error from "../../assets/icons/error.svg";
+import especialista from "../../assets/icons/icon-especialista.svg";
+import autodiagnostico from "../../assets/icons/icon-autodiagnostico.svg";
+import '../../styles/deliveryService/notService.scss';
+import { FaArrowRight } from 'react-icons/fa'
 
 const NotService = () => {
   const history = useHistory();
+  const { patient } = useSelector((state) => state.queries);
+
   return (
-    <div className="laboratory__container text-center">
-      <h3 className="laboratory__header">No tienes servicios pendientes</h3>
-      <img src={noservice} alt="hisopado negativo" />
-      <FooterBtn type='submit' text='Continuar' callback={() => history.push("/")} />
+    <div className="notService__container">
+      <img src={error} alt="ALgo salió mal" className="notService__img" />
+      <h2 className="notService__title">Algo salió mal</h2>
+      <p className="notService__text">No hemos encontrado lo que buscabas, pero tal vez te interese esto</p>
+
+      <button className="notService__button" onClick={() => history.push(`/${patient.ws}/onlinedoctor/who`)}>
+        <div>
+          <img src={especialista} alt="Consulta médica"/>
+          <span>Quiero una consulta médica</span>
+        </div>
+
+        <FaArrowRight />
+      </button>
+
+      <button className="notService__button" onClick={() => history.push(`/${patient.ws}/autonomous`)}>
+        <div>
+          <img src={autodiagnostico} alt="Autodiagnóstico"/>
+          <span>Hacer autodiagnóstico</span>
+        </div>
+
+        <FaArrowRight />
+      </button>
+
+      <button className="notService__link" onClick={() => history.push('/')}>
+        Ir a inicio
+      </button>
     </div>
   )
 }
