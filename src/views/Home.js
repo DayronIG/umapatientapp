4e99504b620a811cdfd2e3b1c49ed7baeab131a4
Farid Murzone <faridmurzone@gmail.com>
2020-11-11 08:10:32 -0300

    deploying

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch} from 'react-redux';
 import Home from '../components/HomePage';
+import {useParams} from 'react-router-dom';
+import { getAuth } from '../store/actions/firebaseQueries';
 
 const HomeView = () => {
+  const dispatch = useDispatch()
+  const {ws} = useParams()
+
+  useEffect(() => {
+    if(ws) {
+      getAuth(ws).then(res => {
+        dispatch({type: 'GET_PATIENT', payload: res})
+      })
+    }
+  }, [ws])
+
   return <Home />
 }
 
