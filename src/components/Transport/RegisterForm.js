c262656ebf0253805ba10345b1b1460dfd782dce
juandeto <juandeto10@gmail.com>
2020-11-13 14:30:51 -0300

    add form transport

--- src/components/Transport/RegisterForm.js
+++ src/components/Transport/RegisterForm.js
@@ -1,26 +1,19 @@
 import React, { useState } from 'react'
 import BackButton from '../GeneralComponents/Backbutton';
-
-import { useHistory, useParams } from 'react-router-dom';
-import Axios from 'axios';
-import swal from 'sweetalert';
-import { transport_register } from '../../config/endpoints';
-import { transport } from '../../config/endpoints';
-import Alert from '../GeneralComponents/Alert/Alerts';
-import { fileToBlob } from '../Utils/fileToBlob';
-import { putFileFB } from '../Utils/firebaseUtils';
 import { YesNoCheckbox, UploadImg, SelectRegister, InputRegister, ContinueButton } from './RegisterFormComponents'
 import '../../styles/transport/registerForm.scss';
 import Cleave from 'cleave.js/react';
-import {CustomUmaLoader} from './../global/Spinner/Loaders';
-import moment from 'moment-timezone';
 
+import { useHistory, useParams } from 'react-router-dom';
+import {CustomUmaLoader} from './../global/Spinner/Loaders';
+import { SendForm } from './SendFormRegister';
+import { useSelector, useDispatch } from 'react-redux';
 
 
 const fromInitialState = {
     dni: '',
     dniPreview: '',
-    discapacity: true,
+    discapacity: false,
     diagnostico: '',
     gradoDiscapacidad: '',
     NroCertificado: '',
@@ -35,79 +28,11 @@ const fromInitialState = {
 
 
 const RegisterForm = () => {
-
-const [loading, setLoading] = useState(false)
+    const dispatch = useDispatch();
+    const { loading } = useSelector(state => state.front);
+        
     const history = useHistory();
     const { ws } = useParams();
-
-
-async function sendForm() {
-    setLoading(true)
-    const t = moment().add(7, 'days');
-    const today = t.valueOf();
-    const t2 = moment().add(7, 'days');
-
-    const date2 = dataForm.VencCertificado.replace(/\//g, '-');
-
-    const date3 = dataForm.VencCertificado.replace(/\//g, '-');
-    const disDate = moment(date3).valueOf();
-try {
-    const date = new RegExp("^([0-2][0-9]||3[0-1])/(0[0-9]||1[0-2])/([0-9][0-9])?[0-9][0-9]$");
-
-    if (dataForm.discapacity === true) {
-        if (!date.test(dataForm.VencCertificado)) {
-            swal('Aviso', 'La fecha que ingresaste es inválida', 'warning');
-            return null;
-        }
-        if (today >= disDate) {
-            swal('Aviso', 'La fecha de vencimiento del certificado es incorrecta', 'warning')
-            return null;
-        }
-    }
-  
-    const licenceBlob = await fileToBlob(dataForm.certImg);
-    const dniBlob = await fileToBlob(dataForm.dni);
-    const getUserData = JSON.parse(localStorage.getItem('userData'));
-    const [url_credential, url_dni] = await Promise.all([
-        putFileFB(dniBlob, `/${getUserData.dni}/dni_photo`),
-        putFileFB(licenceBlob, `/${getUserData.dni}/licence_photo`)
-    ]);
-    const data = {
-        'ws': getUserData.ws,
-        'dni': getUserData.dni,
-        'dt': '',
-        'discapacidad': dataForm.discapacity,
-        'credencial': dataForm.NroCertificado,
-        'silla_ruedas': dataForm.sillaRuedas,
-        'diagnostico': dataForm.diagnostico,
-        'amparo': dataForm.acompañante,
-        'acompanante': dataForm.acompañanteName,
-        'dni_foto': url_dni,
-        'credencial_foto': dataForm.getLicenceFile,
-        'certificado_foto': url_credential,
-        'certificado_discapacidad_vencimiento': dataForm.VencCertificado
-    };
-    const config = { headers: { 'Content-Type': 'application/json;charset=UTF-8'/* , 'Authorization': token */ } };
-    await Axios.post(transport_register, data, config);
-    await swal({
-        title: "Formulario enviado",
-        text: "En breve será redireccionado a la página de inicio",
-        icon: "success",
-        buttons: true,
-        timer: 3000
-                });
-                history.push(`/${getUserData.ws}/transportUserActive`);
-} catch (error) {
-    console.error(error);
-    swal('Error', 'Hubo un error en el envío del Formulario, será redireccionado al registro nuevamente...', 'warning');
-    setTimeout(function () {
-        history.push(`/${ws}/transportRegister`);
-    }, 2000);
-} finally {
-    setLoading(false)
-
-}
-}
 
 
 function buildImages(fileValue, id) {
@@ -117,14 +42,12 @@ function buildImages(fileValue, id) {
     } else {
         imagePreview = URL.createObjectURL(fileValue);
     }
-
-    
     switch (id) {
         case 'dni':
-         setForm({...dataForm, id: fileValue,  dniPreview: imagePreview})   
+         setForm({...dataForm, dni: fileValue,  dniPreview: imagePreview})   
             break;
        case 'certImg':
-         setForm({...dataForm, id: fileValue,  certImgPreview: imagePreview})   
+         setForm({...dataForm, certImg: fileValue,  certImgPreview: imagePreview})   
              break;
         default:
             break;
@@ -137,19 +60,19 @@ function buildImages(fileValue, id) {
     const [dataForm, setForm] = useState(fromInitialState)
 
     const checkboxHandlerDiscapacity = (e) => {
-        setForm({...dataForm, diagnostico: e.target.value})
+        setForm({...dataForm, discapacity: e.target.value === 'true' ? true : false})
     }
 
     const checkboxHandlerSillaRuedas = (e) => {
-        setForm({...dataForm, sillaRuedas: e.target.value})
+        setForm({...dataForm, sillaRuedas: e.target.value === 'true' ? true : false})
     }
     
     const  checkboxHandlerAcompañante= (e) => {
-        setForm({...dataForm, acompañante: e.target.value})
+        setForm({...dataForm, acompañante: e.target.value === 'true' ? true : false})
     }
 
     const selectHandlerTypeDiscapacity = (e) => {
-        setForm({...dataForm, dni: e.target.value})
+        setForm({...dataForm, diagnostico: e.target.value})
     }
 
     const selectHandlerGradoDiscapacidad = (e) => {
@@ -168,8 +91,12 @@ function buildImages(fileValue, id) {
         setForm({...dataForm, acompañanteName: e.target.value})
     }
 
-    const handlerSubmit = (e) => {
-        sendForm();
+    const handlerSubmit = async (e) => {
+        e.preventDefault()
+        dispatch({ type: 'LOADING', payload:true});
+
+       await SendForm(dataForm, history, ws) 
+
     }
 
     if(!loading){
@@ -191,7 +118,8 @@ function buildImages(fileValue, id) {
                     id='discapacity'
                     cb={checkboxHandlerDiscapacity}
                 />
-                {dataForm.discapacity && (
+                {dataForm.discapacity}
+                {dataForm.discapacity ? (
                     <>
                         <SelectRegister
                             title='¿Qué diagnóstico de discapacidad tiene?'
@@ -254,7 +182,7 @@ function buildImages(fileValue, id) {
                             />
                         )}
                     </>
-                )}
+                ):null}
                 <ContinueButton
                     title='Finalizar'
                     type='submit'
--- src/components/Transport/RegisterFormComponents.js
+++ src/components/Transport/RegisterFormComponents.js
@@ -7,10 +7,10 @@ export const YesNoCheckbox = ({ title, id, cb }) => (
             {title}
         </label>
         <span className="checkboxsContainer_cheboxContainer">
-        <input value='1' name={id} type="radio" id={`${id}Yes`} onChange={(e) => cb(e)} />
+        <input value="true" name={id} type="radio" id={`${id}Yes`} onChange={(e) => cb(e)} />
         <label htmlFor={`${id}Yes`}>Si</label></span>
         <span className="checkboxsContainer_cheboxContainer">
-        <input value='0' name={id} type="radio" id={`${id}No`} onChange={(e) => cb(e)} />
+        <input value="false" name={id} type="radio" id={`${id}No`} onChange={(e) => cb(e)} />
         <label htmlFor={`${id}No`}>No</label></span>
     </div>
 )
@@ -21,14 +21,11 @@ export const UploadImg = ({ title, id, cb, previewImage }) => (
             {title}
         </label>
         <span className="uploadImgContainer_fileContainer">
-            { previewImage ? <img className="imagePreview" src={previewImage}/> : null}
-        
-             <div className="uploadImgContainer_uploadTextCont">
+            { previewImage ? <img className="imagePreview" src={previewImage}/> : null} 
+        <div className="uploadImgContainer_uploadTextCont">
         <i className="fas fa-file-upload"></i>
         <p>{ previewImage ? 'Cambiar Foto ': 'Subir Foto' }</p>
-</div> 
-       
-        
+        </div>    
         <input type="file" id={`${id}img`} onChange={(e) => cb(e.target.files[0], id)} />
         </span>
     </div>
@@ -59,8 +56,8 @@ export const InputRegister = ({ title, id, cb }) => (
 
 export const ContinueButton = ({ title, cb, type }) => {
     return (
-        <div className="buttonContainer buttonFinailzar">
-            <button onClick={() => cb()} type={type || ''} >
+        <div className="buttonContainer buttonFinalizar">
+            <button onClick={(e) => cb(e)} type={type || ''} >
                 {title}
             </button>
         </div>
