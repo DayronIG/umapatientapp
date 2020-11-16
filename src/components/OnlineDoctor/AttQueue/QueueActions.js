4b338cf1a8db7c412e446e8a608299f449981085
Valdazoamerico <valdazoamerico@gmail.com>
2020-11-16 10:16:21 -0300

    upload files added

--- src/components/OnlineDoctor/AttQueue/QueueActions.js
+++ src/components/OnlineDoctor/AttQueue/QueueActions.js
@@ -1,12 +1,46 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Alert from '../../GeneralComponents/Alert/Alerts';
 import '../../../styles/questions.scss';
+import { FaFileMedicalAlt } from 'react-icons/fa';
+import { uploadFileToFirebase } from '../../Utils/postBlobFirebase';
+import moment from 'moment-timezone';
+import swal from 'sweetalert';
 
 const QueueActions = (props) => {
 	const dispatch = useDispatch()
-    const [alert, setAlert] = useState(false)
+	const [alert, setAlert] = useState(false)
+	const [contador, setContador] = useState(0);
+	const [loading, setLoading] = useState(false);
+	const [selectedAppointment, setSelectedAppointment] = useState({});
+	const [File, setFile] = useState([]);
+	const auth = useSelector((state) => state.queries.patient);
+
+	useEffect(() => {
+		const data = JSON.parse(localStorage.getItem('selectedAppointment'));
+		setSelectedAppointment(data);
+	}, []);
+
+
+	const uploadImage = e => {
+		setLoading(true);
+		let dt = moment().format('DD-MM-YYYY_HH:mm:ss');
+		let file = e.target.files[0];
+		let fileName = e.target.files[0].name;
+		
+		uploadFileToFirebase(file, `${auth.dni}/attached/${selectedAppointment?.path?.split('/')?.[3]}/${dt}_${fileName}`)
+			.then(imgLink => {
+				setContador(contador + 1);
+				setFile([...File, imgLink]);
+				setLoading(false);
+				swal('Éxito', 'Archivo cargado exitosamente', 'success');
+			})
+			.catch(() => {
+				setLoading(false);
+				swal('Error', 'Hubo un error al adjuntar el archivo, intente nuevamente', 'error');
+			})
+	}
 
     return (
 		<div className="questionsContainer">
@@ -27,6 +61,14 @@ const QueueActions = (props) => {
 					<button className="btn-questions btn-calling" onClick={() => setAlert(true)}>Ingresar al consultorio</button>
 				</Link>
 			}
+
+			<div className="input-file">
+					<FaFileMedicalAlt size="1.5rem" />
+					<p>{ contador < 1 ? 'Adjuntar archivo' : ( contador === 1 ? `${contador} archivo adjunto` : `${contador} archivos adjuntos` ) }</p>
+					<input type="file" onChange={uploadImage} />
+			</div>
+
+
 			{	
 				props.appState !== 'ATT' && props.appState !== 'DONE' && !props.calling && 
 				<button className="btn-questions btn-alert" onClick={() => setAlert(true)}>Cancelar consulta</button>
