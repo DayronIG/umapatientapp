0873b87334f92a4e0d7c351dd93d6730fe6a4208
santiago-rodriguezs <srodriguez@emergencias.com.ar>
2020-11-17 09:55:14 -0300

    fixed

--- src/components/DeliveryService/BuyButton/ButtonAllHisopados.js
+++ src/components/DeliveryService/BuyButton/ButtonAllHisopados.js
@@ -5,8 +5,6 @@ import {FaArrowRight} from "react-icons/fa"
 export default function ButtonStyle({finalAction}) {
     const quantity = useSelector((state) => state.deliveryService.deliveryInfo).filter(el => !["FREE", "FREE:IN_RANGE", "DEPENDANT"].includes(el.status)).length
 
-    console.log(quantity)
-    
     return (
         <section className="all_hisopado__container" onClick={() => finalAction()}>
         <div className="all_hisopado__content">
