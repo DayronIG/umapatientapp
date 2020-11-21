039dc6bc4b4862eaa458ddc6995b00fc0857b927
Farid Murzone <faridmurzone@gmail.com>
2020-11-21 08:29:24 -0300

    funca barbaro el pago

--- src/components/Payment/paymentCardMP.js
+++ src/components/Payment/paymentCardMP.js
@@ -29,8 +29,8 @@ const PaymentCardMP = () => {
     const [creditCard, setCreditCard] = useState("");
     const [invalidYear, setInvalidYear] = useState(false);
     const discountParam = useSelector(state => state.deliveryService.params.discount)
-    // const MERCADOPAGO_PUBLIC_KEY = 'TEST-f7f404fb-d7d3-4c26-9ed4-bdff901c8231';
-    const MERCADOPAGO_PUBLIC_KEY = "APP_USR-e4b12d23-e4c0-44c8-bf3e-6a93d18a4fc9";
+    const MERCADOPAGO_PUBLIC_KEY = 'TEST-f7f404fb-d7d3-4c26-9ed4-bdff901c8231';
+    // const MERCADOPAGO_PUBLIC_KEY = "APP_USR-e4b12d23-e4c0-44c8-bf3e-6a93d18a4fc9";
     const [allPurchases, setAllPurchases] = useState([])
 
 
@@ -156,7 +156,7 @@ const PaymentCardMP = () => {
          axios.post(payment_url, paymentData, {headers})
              .then(res => {
                 setLoader(false)
-                 if (res.data.body.status === "approved") {
+                 if (res.data.body?.status === "approved") {
                     window.gtag('event', 'purchase', {
                       'transaction_id': current.id,
                       'affiliation': user?.corporate_norm,
@@ -174,7 +174,9 @@ const PaymentCardMP = () => {
                         'transaction_id': current.id
                       });
                      setStatus("approved")
-                } else if (res.data.body.status === "rejected") {
+                  } else if (res.data.body.status === "free") {
+                    setStatus("approved")
+                  } else if (res.data.body.status === "rejected") {
                   window.gtag('event', 'payment_failed', {
                     'event_category' : 'warning',
                     'event_label' : 'hisopado_payment'
@@ -186,6 +188,7 @@ const PaymentCardMP = () => {
             })
             .catch(err => {
               setLoader(false)
+              console.error(err)
               window.gtag('event', 'payment_failed', {
                 'event_category' : 'warning',
                 'event_label' : 'hisopado_payment'
@@ -217,7 +220,7 @@ const PaymentCardMP = () => {
           setLoader(false)
           dispatch({type: 'SET_DELIVERY_STEP', payload: "END_ASSIGNATION"})
           swal('El pago se ha registrado correctamente', 'Gracias por confiar en ÜMA!', 'success')
-            .then(()=> history.push("/"))
+            .then(()=> history.push(`/hisopado/listTracker/${user.ws}`))
         } else if(paymentStatus && paymentStatus !== "approved" && paymentStatus !== "") {
             switch(statusDetail){
               case("cc_rejected_insufficient_amount"):
