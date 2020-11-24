0a72015b6245d784dd3d6947887d3590f4f5a152
santiago-rodriguezs <srodriguez@emergencias.com.ar>
2020-11-24 12:58:57 -0300

    pastillero

--- src/components/Pillbox/Pillbox.js
+++ src/components/Pillbox/Pillbox.js
@@ -15,6 +15,31 @@ const Pillbox = props => {
     const [dates, setDates] = useState([]);
     const [modal, setModal] = useState(false);
 
+    const recipes = [{
+        uid: "",
+        treatment: {
+            dose: 1,
+            frequency_hours: 3,
+            quantity_days: 5,
+        },
+        medicine: "AMOXIDAL",
+        notify: true,
+        initial_date: "2020-12-12",
+        final_date: "2020-05-12"
+    },
+    {
+        uid: "",
+        treatment: {
+            dose: 1,
+            frequency_hours: 3,
+            quantity_days: 5,
+        },
+        medicine: "AMOXIDAL",
+        notify: true,
+        initial_date: "2020-12-12",
+        final_date: "2020-05-12"
+    }]
+
     const disabledDate = current => {
     //   if (!dates || dates.length === 0) {
     //     return false;
@@ -30,25 +55,24 @@ const Pillbox = props => {
 
     const recipesList = () => {
         const recipeList = [];
-        // for (const [index, recipe] of data.diagnostics.recipe.medicines.entries()) {
-        //     recipeList.push(
-        //         <div className='d-flex bd-highlight' key={index}>
-        //             <div className='p-2 flex-grow-1 bd-highlight'>
-        //                 <span className='d-block recipe__drug'>{recipe.drug}</span>
-        //                 <span className='recipe__treatment'>{recipe.treatment.quantity_per_day} cada {recipe.treatment.periodicity} horas por {recipe.treatment.days} días</span>
-        //             </div>
-        //             <div className='p-2 bd-highlight'>
-        //                 {console.log(recipe.reminder.active)}
-        //             {/* <Switch
-        //                 checkedChildren={<CheckOutlined />}
-        //                 unCheckedChildren={<CloseOutlined />}
-        //                 onClick={(checked) => handleReminder(recipe, checked, index)}
-        //                 checked = {recipe.reminder.active}
-        //                 /> */}
-        //             </div>
-        //         </div>
-        //     )
-        // }
+        for(var i = 0; i < recipes.length; i++) {
+            recipeList.push(
+                <div className='recipesList__container' key={i}>
+                    <div className='recipesListIndicator__container'>
+                        <span className=''>{recipes[i].medicine}</span>
+                        <span className=''>{recipes[i].treatment.dose} cada {recipes[i].treatment.frecuency_hours} horas por {recipes[i].treatment.quantity_days} días</span>
+                    </div>
+                    <div className='recipesListSwitch__container'>
+                    {/* <Switch
+                        checkedChildren={<CheckOutlined />}
+                        unCheckedChildren={<CloseOutlined />}
+                        onClick={(checked) => handleReminder(recipe, checked, index)}
+                        checked = {recipe.reminder.active}
+                        /> */}
+                    </div>
+                </div>
+            )
+        }
         return recipeList
     }
 
@@ -57,9 +81,9 @@ const Pillbox = props => {
         {modal && <Modal
           title="Monitoreo"
           callback={setModal(true)}>
-              <div md={12} className='mt-4 text-center'>
-                    <h2 className='text__section mt-2 mb-3'>Recordatorio</h2>
-                    <div md={12} className='mt-4 range-picker-container'>
+              <div className=''>
+                    <h2 className=''>Recordatorio</h2>
+                    <div className=''>
                         <input
                         disabledDate={disabledDate}
                         className="range-picker"
@@ -78,21 +102,21 @@ const Pillbox = props => {
                         />
                     </div>
                     <input
-                        className='mt-3'
+                        className=''
                         format={format}
                         placeholder="Start time"
                     />
-                    <div className="button__footer mt-5">
+                    <div className="">
                         <button
                             variant='primary'
-                            className='index__button mt-2 mb-3'
+                            className=''
                             // onClick={() => props.hide(recipe?.index, true)} 
                             >
                             Añadir Recordatorio
                         </button>
                         <button
                             variant='primary' 
-                            className='index__button status' 
+                            className='' 
                             // onClick={() => props.hide(recipe?.index, false)} 
                             >
                             Cancelar
@@ -100,46 +124,24 @@ const Pillbox = props => {
                     </div>
                 </div>
             </Modal>}
-            <div className='pillboxList__container'>
+            <div className=''>
                 <h2 className="pillbox__title">Pastillero Uma</h2>
-                <div md={12}>
-                    <div className='d-flex bd-highlight'>
-                        <div className='p-2 flex-grow-1 bd-highlight recipe__drug'>
+                <div className='pillboxList__container'>
+                    <div className="">
+                        <h2 className='pillbox__title'>Monitoreo</h2>
+                    </div>
+                    <div className='pillboxReminder__header'>
+                        <div className=''>
                             Receta
                         </div>
-                    </div>
-                    <div className='d-flex bd-highlight'>
-                    <div className='p-2 flex-grow-1 bd-highlight'>
-                        <span className='d-block recipe__drug'>{"AMOXIDAL"}</span>
-                        <span className='recipe__treatment'>
-                        {1} cada {1} horas por {1}
-                        días</span>
-                    </div>
-                </div>
-                <div md={12} className=' mt-4'>
-                    {/* mt-3 */}
-                    <div className="d-flex bd-highlight mb-5 ">
-                        <div className="p-2 bd-highlight">
-                            {/* <img src={img} className='ml-3' alt=""/> */}
-                        </div>
-                        <div className="p-2 ">
-                            <h2 className=' mt-2'>Monitoreo</h2>
-                        </div>
-                    </div>
-                    <div className='d-flex bd-highlight'>
-                        <div className='p-2 flex-grow-1 bd-highlight'>
-                            Receta
-                        </div>
-                        <div className='p-2 bd-highlight'>
+                        <div className=''>
                             Recordatorio
                         </div>
                     </div>
                     {recipesList()}
                 </div>
                 </div>
-                
             </div>
-        </div>
     )
 
 }
