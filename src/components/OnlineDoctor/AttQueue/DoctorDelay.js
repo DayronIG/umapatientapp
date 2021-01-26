f710936b4e63d8aa96a5d551da25e028f4c98aaf
cristian <cmorales@emergencias.com.ar>
2021-01-26 16:22:03 -0300

    algo

--- src/components/OnlineDoctor/AttQueue/DoctorDelay.js
+++ src/components/OnlineDoctor/AttQueue/DoctorDelay.js
@@ -1,43 +1,43 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import moment from 'moment';
 import { getDocumentsByFilter } from '../../Utils/firebaseUtils';
 
-const DoctorDelay = ({cuit, date, time}) => {
+const DoctorDelay = ({ cuit, date, time }) => {
     const [queue, setQueue] = useState('0');
     const [delay, setDelay] = useState('5');
 
     useEffect(() => {
         getDelayAndQueue()
-		let interval = setInterval(()=> {
+        let interval = setInterval(() => {
             getDelayAndQueue()
         }, 60000)
         return () => interval
     }, [cuit]);
 
     const getDelayAndQueue = useCallback(() => {
-        if(cuit && cuit !== ''){
-			let dt = moment().format('YYYYMM');
-			let filters = [
-				{field: 'cuit', value: cuit, comparator: '=='},
-				{field: 'status', value: cuit, comparator: 'ASSIGN'}			
-			]
-			getDocumentsByFilter(`/assignations/online_clinica_medica/${dt}`, filters)
-				.then(res => {
-					setQueue(res.length)
+        if (cuit && cuit !== '') {
+            let dt = moment().format('YYYYMM');
+            let filters = [
+                { field: 'cuit', value: cuit, comparator: '==' },
+                { field: 'status', value: 'ASSIGN', comparator: '==' }
+            ]
+            getDocumentsByFilter(`/assignations/online_clinica_medica/${dt}`, filters)
+                .then(res => {
+                    setQueue(res.length)
                     let pendingTime = moment(`${date} ${time}:00`).diff(new Date(), 'minutes')
-                    if(pendingTime <= 0) {
+                    if (pendingTime <= 0) {
                         pendingTime = 5
                     }
-					console.log(pendingTime)
-					if(res.length >= 1){ 
-						setDelay(res.length * 10 + pendingTime)
-					} else {
-						setDelay(pendingTime)
-					}
-				})
-		}
+                    console.log(pendingTime)
+                    if (res.length >= 1) {
+                        setDelay(res.length * 10 + pendingTime)
+                    } else {
+                        setDelay(pendingTime)
+                    }
+                })
+        }
     }, [cuit, date, time])
-    
+
     return <div className="appointment__delay--container">
         <div className="appointment__delay">
             <span className="appointment__number">{queue}</span>
