45e7d141c7d0a0785c4e7d909f3bac5c25c0b089
Sofia Lay <sofi-lay@hotmail.com>
2021-01-28 12:16:06 -0300

    beneficiary styles changes

--- src/components/MyRecords/MyRecords.js
+++ src/components/MyRecords/MyRecords.js
@@ -7,7 +7,7 @@ import { faUserMd } from '@fortawesome/free-solid-svg-icons';
 import { HistoryHeader } from '../GeneralComponents/Headers';
 import { getMedicalRecord } from '../../store/actions/firebaseQueries';
 import { FaSlidersH } from "react-icons/fa";
-import '../../styles/history/myrecords.scss';
+import '../../styles/history/MyRecords.scss';
 
 const MyRecords = () => {
     const {category} = useParams();
@@ -33,19 +33,18 @@ const MyRecords = () => {
 
     return (
         <>
-            
             <HistoryHeader> Consultas </HistoryHeader>
             <div className="my-history-container"> 
                 <div className="title-icon">
                     <p className="font-weight-bold">Consultas médicas</p>
-                    <FaSlidersH/>
+                    <button><FaSlidersH/></button>
                 </div>
                 {/*  Beneficiary cambia de lugar */}
                 <div className="my-history-beneficiary"> 
-                    <button className={tab === patient.fullname ? "active btn btn-secondary" : "btn btn-secondary"} 
+                    <button className={tab === patient.fullname ? "active button-patient" : "button-patient"} 
                             onClick={() => selectBeneficiarieMr("owner")}> {patient.fullname} </button>
                     {beneficiaries.map((p, index) => {
-                        return <button className={tab === p.fullname ? "active btn btn-secondary" : "btn btn-secondary"}
+                        return <button className={tab === p.fullname ? "active button-patient" : "btn button-patient"}
                             onClick={() => selectBeneficiarieMr(p)}
                             key={index}> {p.fullname} </button>
                     })}
@@ -57,67 +56,65 @@ const MyRecords = () => {
                         return ( 
                             r.mr.destino_final !== "USER CANCEL" &&
                             (r.mr.destino_final !== "" || r.incidente_id !== 'auto') &&
-                            <li key={index} className="my-history-consultation">
-                                {/*</li>className={r.incidente_id === 'DISCA' ? "transport myhistory-consultation" : (r.incidente_id === 'auto' ? "myhistory-consultation history-bg-autonomous" : "myhistory-consultation")*/}
-                                {/*{r.incidente_id !== 'auto' ?*/}
-                                {/* ${r.patient.dni}/${r.assignation_id} */}
-                                    <Link to={`/${r.patient.ws}/history/${r.patient.dni}/${r.assignation_id}`} className="consult-link">
+                            <>
+                                <li key={index} className="my-history-consultation">
+                                    {/*</li>className={r.incidente_id === 'DISCA' ? "transport myhistory-consultation" : (r.incidente_id === 'auto' ? "myhistory-consultation history-bg-autonomous" : "myhistory-consultation")*/}
+                                    {/*{r.incidente_id !== 'auto' ?*/}
+                                    {/* ${r.patient.dni}/${r.assignation_id} */}
+                                        <Link to={`/${r.patient.ws}/history/${r.patient.dni}/${r.assignation_id}`} className="consult-link">
 
-                                        {/* <div className=""> */}
-                                            <div className="left-icon">
-                                                <FontAwesomeIcon icon={faUserMd} />
-                                            </div>
-                                            <div className="title-date"> 
-                                                <p className="title">Guardia</p>
-                                                <p className="consult-date">{!!r.mr && moment(r.mr.dt_cierre).format('DD-MM-YYYY')}</p>
-                                            </div>
-                                        {/* </div> */}
-                                        
-
-                                        {/*{r.incidente_id === 'DISCA' ?
-                                            <div className="leftIcon">
-                                                <FontAwesomeIcon icon={faCarAlt} />
-                                            </div>
-                                            :
-                                            <div className="leftIcon">
-                                                <FontAwesomeIcon icon={faUserNurse} />
-                                            </div>
-                                        }
-                                        <div>
-                                            <div className="consultContainer"> 
-                                                <p>Guardia</p>
-                                                <p className="font-weight-normal">{!!r.mr && moment(r.mr.dt_cierre).format('DD-MM-YYYY')}</p>
-                                            </div>
-                                            {r.incidente_id === 'DISCA' ?
-                                                <div>
-                                                    <p>Traslado</p>
+                                                <div className="left-icon">
+                                                    <FontAwesomeIcon icon={faUserMd} />
                                                 </div>
-                                                : r.mr.motivos_de_consulta &&
-                                                <div className="wrapper-consultContainer">
-                                                    <p>Consulta por {r.mr.motivos_de_consulta}</p>
+                                                <div className="title-date"> 
+                                                    <p className="title">Guardia</p>
+                                                    <p className="consult-date">{!!r.mr && moment(r.mr.dt_cierre).format('DD-MM-YYYY')}</p>
+                                                </div>
+
+                                            {/*{r.incidente_id === 'DISCA' ?
+                                                <div className="leftIcon">
+                                                    <FontAwesomeIcon icon={faCarAlt} />
+                                                </div>
+                                                :
+                                                <div className="leftIcon">
+                                                    <FontAwesomeIcon icon={faUserNurse} />
                                                 </div>
                                             }
-                                        </div>*/}
-                                        
-                                    </Link>
-                                    <hr/>
-                                   {/* :
-                                    <div className="my-autonomous d-flex ">
-                                        <div className="leftIcon">
-                                            <i className="fas fa-vr-cardboard"></i>
-                                        </div>
-                                        <div>
-                                            <div className="consultContainer">
-                                                {!!r.mr && r.mr.dt_cierre}
+                                            <div>
+                                                <div className="consultContainer"> 
+                                                    <p>Guardia</p>
+                                                    <p className="font-weight-normal">{!!r.mr && moment(r.mr.dt_cierre).format('DD-MM-YYYY')}</p>
+                                                </div>
+                                                {r.incidente_id === 'DISCA' ?
+                                                    <div>
+                                                        <p>Traslado</p>
+                                                    </div>
+                                                    : r.mr.motivos_de_consulta &&
+                                                    <div className="wrapper-consultContainer">
+                                                        <p>Consulta por {r.mr.motivos_de_consulta}</p>
+                                                    </div>
+                                                }
+                                            </div>*/}
+                                            
+                                        </Link>
+                                    {/* :
+                                        <div className="my-autonomous d-flex ">
+                                            <div className="leftIcon">
+                                                <i className="fas fa-vr-cardboard"></i>
                                             </div>
-                                            <div className="wrapper-consultContainer">
-                                                <p>Autonomous: {r.mr.epicrisis}</p>
+                                            <div>
+                                                <div className="consultContainer">
+                                                    {!!r.mr && r.mr.dt_cierre}
+                                                </div>
+                                                <div className="wrapper-consultContainer">
+                                                    <p>Autonomous: {r.mr.epicrisis}</p>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                   }*/}
-                                    
-                            </li>
+                                    }*/} 
+                                </li>
+                                <hr/>
+                            </>
                         )
                     })}
                 </ul>
