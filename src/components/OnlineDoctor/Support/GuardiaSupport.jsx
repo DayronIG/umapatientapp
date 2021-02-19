ed24ef65bf6597b93620a266879cacd903120395
Farid <faridmurzone@gmail.com>
2021-02-19 17:53:58 -0300

    Funciona el complain y cancel

--- src/components/OnlineDoctor/Support/GuardiaSupport.jsx
+++ src/components/OnlineDoctor/Support/GuardiaSupport.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FaAngleRight} from 'react-icons/fa';
 import SendComplain from './actions/MakeComplain';
 import CancelAppointment from './actions/CancelAppointment';
 
 const GuardiaSupport = () =>{
+    const [active, setActive] = useState('')
+
     return (<>
-        {<SendComplain />}
-        {/* <CancelAppointment /> */}
-        <div className="issue__container">
+        {active === "cancel" && <CancelAppointment /> }
+        {active === "complain" && <SendComplain />}
+        <div className="issue__container" onClick={() => setActive("complain")}>
             <p className="issue__title">Deseo realizar un reclamo</p>
             <FaAngleRight  className="issue__icon" />
         </div>
-        <div className="issue__container">
+        <div className="issue__container" onClick={() => setActive("cancel")}>
             <p className="issue__title">Deseo cancelar mi turno</p>
             <FaAngleRight  className="issue__icon" />
         </div>
--- src/components/OnlineDoctor/Support/actions/MakeComplain.jsx
+++ src/components/OnlineDoctor/Support/actions/MakeComplain.jsx
@@ -1,27 +1,22 @@
 import React, { useState } from 'react';
 import {useDispatch, useSelector} from 'react-redux';
+import {useLocation, useHistory} from 'react-router-dom';
 import swalReact from '@sweetalert/with-react';
 import swal from 'sweetalert';
-import AnswerComplain from '../../../CX';
-import MobileModal from '../../../GeneralComponents/Modal/MobileModal';
 import { cx_action_create } from '../../../../config/endpoints';
 import axios from 'axios';
 import moment from 'moment';
-import Satisfy from './Actions/Satisfy'
-import MakeAppointment from './Actions/MakeAppointment';
-import PhoneCall from './Actions/PhoneCall';
-import Autonomus from './Actions/Autonomus';
-import Reassign from './Actions/Reassign';
-import Derivate from './Actions/Derivate';
-import Default from './Actions/Default';
+import queryString from 'query-string';
+
 
 const SendComplain = ({ sendComplain }) => {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    const history = useHistory();
     const [claim, setClaim] = useState('');
     const patient = useSelector(state => state.user)
-    const [responseAction, setResponseAction] = useState({ response: '', action: '' })
 	const { currentUser } = useSelector((state) => state.userActive)
-    const { assignedAppointment } = useSelector(state => state.queries)
+    const location = useLocation();
+    const {id} = queryString.parse(location.search)
 
     const onSubmitButton = () => {
         if (!claim.trim()) {
@@ -32,17 +27,15 @@ const SendComplain = ({ sendComplain }) => {
         )
         return;
         }
-        sendComplain(claim);
+        handleComplain(claim);
     }
 
-    async function handleComplain(type, claim) {
-        dispatch({ type: 'TOGGLE_MODAL_ACTION', payload: true })
-        dispatch({ type: 'TOGGLE_DETAIL' });
+    async function handleComplain(claim) {
         let data = {
             ws: patient.ws,
             dni: patient.dni,
             dt: moment().format('YYYY-MM-DD HH:mm:ss'),
-            assignation_id: assignedAppointment.appointments[0][14] || patient.incidente_id,
+            assignation_id: id,
             appointment_path: '',
             type: 'complain',
             complain: claim
@@ -50,9 +43,9 @@ const SendComplain = ({ sendComplain }) => {
         try {
             currentUser.getIdToken().then(async token => {
                 let headers = { 'Content-Type': 'Application/Json', 'Authorization': token }
-                const res = await axios.post(cx_action_create, data, headers)
-                setResponseAction({ response: res.data.ai_response, action: res.data.ai_action })
-                dispatch({type: 'SET_CX_RESPONSE', payload: res.data})
+                await axios.post(cx_action_create, data, headers)
+                swal('Enviado', 'El reclamo fué enviado con éxito. Será evaluado por nuestro equipo.', 'success')
+                return history.push(`/onlinedoctor/queue/${patient.dni}`)
             })
         } catch (error) {
             dispatch({ type: 'TOGGLE_MODAL_ACTION', payload: false })
@@ -60,15 +53,14 @@ const SendComplain = ({ sendComplain }) => {
         }
     }
 
-
     return (
-        <>
-        <AnswerComplain responseAction={responseAction} />
-        <div className='detail-modal-content'>
-            <textarea placeholder='Escriba aquí el motivo de su reclamo' onChange={(e) => setClaim(e.target.value)}></textarea>
-            <button className='btn btn-blue-lg' onClick={handleComplain}>Enviar reclamo</button>
+        <div className="action__container">
+            <div className='detail-modal-content'>
+            <h2>Por favor detalla lo mejor posible el problema</h2>
+                <textarea placeholder='Escriba aquí el motivo de su reclamo' onChange={(e) => setClaim(e.target.value)}></textarea>
+                <button className='btn btn-blue-lg' onClick={onSubmitButton}>Enviar reclamo</button>
+            </div>
         </div>
-        </>
     )
 }
 
