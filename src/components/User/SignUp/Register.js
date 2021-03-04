2634ae88617f66378f99a990bc80917776635727
sofialay <sofi-lay@hotmail.com>
2021-03-04 12:02:55 -0300

    fix del fix

--- src/components/User/SignUp/Register.js
+++ src/components/User/SignUp/Register.js
@@ -273,9 +273,9 @@ const Register = () => {
                         <GenericInputs label='Ingresa tu número de identidad' type='number' name='dni' validate={(e) =>handleInputsValidations(e)} />
                         <GenericInputs label='Ingresa tu numero de celular' type='number' name='phone' validate={(e) =>handleInputsValidations(e)}/>
                         <GenericInputs label='¿Cual es tu cobertura de salud?' type='text' name='healthinsurance' validate={(e) =>handleInputsValidations(e)}/>
+                        <SelectOption select action={(e) => getSexValue(e)}/>
                         <SelectOption calendar action={(e)=>handleDate(e)}/>
                         {errorDataRegister.map(item => item === 'Fecha de nacimiento' && <p className='invalid-field date'>Debes ser mayor de 16 años para poder utilizar la aplicación</p>)}
-                        <SelectOption select action={(e) => getSexValue(e)}/>
                     </> 
                     }
                 </form>
