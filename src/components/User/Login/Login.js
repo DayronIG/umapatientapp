62654fa80dd150e0be88a0cddbf4662b2c67227f
sofialay <sofi-lay@hotmail.com>
2021-03-04 12:36:49 -0300

    fixes en el login

--- src/components/User/Login/Login.js
+++ src/components/User/Login/Login.js
@@ -9,48 +9,14 @@ import '../../../styles/user/login.scss';
 
 const Login = () =>  {
     const history = useHistory();
-    const dispatch = useDispatch();
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const [errorData, setErrorData] = useState([]);
-    const [validations, setValidations] = useState([{
-        email: false,
-        password: false
-    }])
+    const [errorData, setErrorData] = useState(false);
 
     const handleSignIn = async () => {
-        if(validations.email && validations.password) {
-            await Firebase.auth().signInWithEmailAndPassword(email, password);
-            dispatch({ type: 'USER_PASSWORD', payload: '' });
-        } else {
-            if(!validations.email) {
-                setErrorData((errorData) =>[...errorData,'Email'])
-            }
-            if(!validations.password) {
-                setErrorData((errorData) =>[...errorData,'Contraseña'])
-            }
-        }
-    }
-
-    const handleInputsValidations = (e) => {
-        if(e.target.name === 'email') {
-            let validEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(e.target.value)
-            if(validEmail) { 
-                setEmail(e.target.value)
-                setValidations({ ...validations, email: true })
-            } else {
-                setValidations({ ...validations, email: false })
-            }
-        } else if(e.target.name === 'pass') {
-            let validPass = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(e.target.value)
-            if (validPass) {
-                setPassword(e.target.value)
-                setValidations({ ...validations, password: true })
-            } else {
-                setPassword(e.target.value)
-                setValidations({ ...validations, password: false })
-            }
-        }
+        await Firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(()=>setErrorData(false))
+            .catch(()=>setErrorData(true))
     }
 
     return (
@@ -62,14 +28,14 @@ const Login = () =>  {
            </section>
            <LoginButtons circleBtn />
             <div className='login__lineSeparator'>o</div>
-            {errorData.length !== 0 &&
+            {errorData &&
                 <section className='login__invalid'>
                     <p>El mail o la contraseña son incorrectos.</p>
                     <p>Comprueba los datos ingresados o <a className='login__invalid--redirect' onClick={() => history.push('/signup')}>crea una cuenta</a></p>
                 </section>
             }
-            <GenericInputs label='Ingresa tu mail' type='email' name='email' validate={(e) =>handleInputsValidations(e)}/>
-            <GenericInputs label='Ingresa tu contraseña' type='password' name='pass' validate={(e) =>handleInputsValidations(e)} />
+            <GenericInputs label='Ingresa tu mail' type='email' name='email' validate={(e) =>setEmail(e.target.value)}/>
+            <GenericInputs label='Ingresa tu contraseña' type='password' name='pass' validate={(e) =>setPassword(e.target.value)} />
            <section className='login__needHelp'>
                 {/* <aside className='login__needHelp__activeSession'>
                     <input className='check' type='checkbox'/>
