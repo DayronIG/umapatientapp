8abed1dd7e55b1ca57e6bda50603c5a17b08535a
Farid <faridmurzone@gmail.com>
2021-04-05 17:51:45 -0300

    advice fix styles

--- src/components/OnlineDoctor/AttQueue/Advice.js
+++ src/components/OnlineDoctor/AttQueue/Advice.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {useSelector} from "react-redux";
-import {FaInfoCircle} from "react-icons/fa";
 
 export default function Advice() {
     const {guardia_advice} = useSelector((state) => state.front)
@@ -8,7 +7,7 @@ export default function Advice() {
     return (
         <section className="advice__container">
         <div className="advice__content">
-            {guardia_advice && guardia_advice} <FaInfoCircle className="icon" />
+            {guardia_advice && guardia_advice} {/* <FaInfoCircle className="icon" /> */}
         </div>
         </section>
     )
