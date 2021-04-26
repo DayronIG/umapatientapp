67a07c0cbe074be7e9942950260c2cb3b1c44f4d
federicomiranda <federico.mirandaa@gmail.com>
2021-04-26 16:53:47 -0300

    refactor de paymentStatus

--- src/components/Payment/PaymentStatus.js
+++ src/components/Payment/PaymentStatus.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useHistory } from 'react-router';
 import { useSelector, useDispatch } from 'react-redux';
 import { GenericHeader } from '../GeneralComponents/Headers';
@@ -7,8 +7,7 @@ import '../../styles/payments/PaymentStatus.scss';
 import crossIcon from '../../../src/assets/img/hisopados_cross.svg';
 import payedIcon from '../../../src/assets/img/hisopados_payment.svg';
 import queryString from 'query-string';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClock } from '@fortawesome/free-solid-svg-icons';
+import { FaClock } from 'react-icons/fa'
 
 const PaymentStatus = () => {
     const history = useHistory();
@@ -16,23 +15,75 @@ const PaymentStatus = () => {
     const dispatch = useDispatch();
 	const { currentUser } = useSelector((state) => state.userActive);
     const { mercadoPago } = useSelector((state) => state.payments);
-    const { paid, dependant, product, method, amount } = queryString.parse(location.search);
+    const { paid, dependant, product, method, amount, service } = queryString.parse(location.search);
+    const [message, setMessage] = useState({
+        status: '',
+        header: '',
+        icon: '',
+        title: '',
+        paragraphs: [],
+        button: '',
+    })
+
+    useEffect(() => {
+        if(!mercadoPago) {
+            setMessage({
+                status: '',
+                header: 'Falta de pago',
+                icon: crossIcon,
+                title: 'Debes realizar un pago para obtener el servicio',
+                paragraphs: ['El link que utilizaste ya caducó.'],
+                button: 'Ir al inicio',
+            })
+        } else {
+            switch (paid) {
+                case 'rejected':
+                    setMessage({
+                        status: 'rejected',
+                        header: 'Pago Rechazado',
+                        icon: crossIcon,
+                        title: '¡Lo sentimos!',
+                        paragraphs: ['Ha ocurrido un error y el pago fue rechazado por MercadoPago.', 'No te preocupes, no hemos cobrado ningún monto. Por favor, intenta nuevamente.'],
+                        button: 'Intentar de nuevo',
+                    })
+                break;
+                case 'pending':
+                    setMessage({
+                        status: 'pending',
+                        header: 'Pago Pendiente',
+                        icon: <FaClock style={{ color: '#ff9933' }} />,
+                        title: 'El pago esta siendo procesado',
+                        paragraphs: service !== 'hisopado' ? ['Cuando el pago sea confirmado te enviaremos una notificación con un link para continuar con el proceso de la consulta medica.'] : ['Cuando el pago sea confirmado te enviaremos una notificación para que puedas continuar'],
+                        button: 'Ir al inicio',
+                    })
+                break;
+                case 'true':
+                    setMessage({
+                        status: 'true',
+                        header: 'Pago Confirmado',
+                        icon: payedIcon,
+                        title: '¡Hemos recibo el pago!',
+                        paragraphs: service !== 'hisopado' ? ['Toca en "Continuar" para seguir con el proceso de la consulta medica.'] : ['Ya puedes acercarte al consultorio seleccionado para realizar el test'],
+                        button: 'Continuar',
+                    })
+                break;
+                default:
+                    break;
+            }
+        }
+    }, [mercadoPago, paid])
     
     const handleClick = () => {
+        window.gtag('event', `${product}_payment_${paid === 'true' ? 'success' : paid }`)
+        window.gtag('event', `${product}_payment_method`, {
+            'method': method,
+            'status': `${paid === 'true' ? 'success' : paid }`
+        })
+
         if(paid === 'rejected') {
-            window.gtag('event', `${product}_payment_rejected`)
-            window.gtag('event', `${product}_payment_method`, {
-                'method': method,
-                'status': 'rejected'
-            })
             history.push(`/payments/checkout/${currentUser.uid}`)
         }   
         if(paid === 'true') {
-            window.gtag('event', `${product}_payment_success`)
-            window.gtag('event', `${product}_payment_method`, {
-                'method': method,
-                'status': 'success'
-            })
             window.gtag('event', 'earn_virtual_currency', {
                 'virtual_currency_name': 'uma_creditos',
                 'value': amount
@@ -42,11 +93,7 @@ const PaymentStatus = () => {
             history.push(`/onlinedoctor/reason/${currentUser.uid}?dependant=${dependant}?paid=true`)
         }
         if(paid === 'pending') {
-            window.gtag('event', `${product}_payment_pending`)
-            window.gtag('event', `${product}_payment_method`, {
-                'method': method,
-                'status': 'pending'
-            })
+            history.push('/')
         }
     }
 
@@ -62,68 +109,31 @@ const PaymentStatus = () => {
 
     return (
         <>
-        {paid === 'rejected' && mercadoPago &&
             <div className='rejected-payment'>
-                <GenericHeader>Pago Rechazado</GenericHeader>
+                <GenericHeader>{message.header}</GenericHeader>
                 <div className='rejected-icon'>
-                    <img src={crossIcon} alt='crossIcon'/>
+                    {
+                        paid === 'pending' ?
+                        message.icon :
+                        <img src={message.icon} alt='crossIcon' />
+                    }
                 </div>
                 <div className='rejected-payment-body'>
-                    <h3 className='rejected-payment-body-title-rejected'>¡Lo sentimos!</h3>
+                    <h3 
+                        className={`rejected-payment-body-title-${!message.status ? 'rejected' : message.status === 'true' ? 'confirmed' : message.status}`}
+                    >
+                        {message.title}
+                    </h3>
                     <div className='rejected-payment-body-text-container'>
-                        <p className='rejected-payment-body-text'>Ha ocurrido un error y el pago fue rechazado por MercadoPago.</p>
-                        <p className='rejected-payment-body-text'>No te preocupes, no hemos cobrado ningún monto. Por favor, intenta nuevamente.</p>
+                        {
+                            message.paragraphs.map((p, index) => (
+                                <p key={index} className='rejected-payment-body-text'>{p}</p>
+                            ))
+                        }
                     </div>
-                    <button className='btn btn-blue-lg mb-5' onClick={handleClick}>Intentar de nuevo</button>
+                    <button className='btn btn-blue-lg mb-5' onClick={handleClick}>{message.button}</button>
                 </div>
             </div>
-        }
-        {paid === 'pending' && mercadoPago &&
-            <div className='rejected-payment'>
-                <GenericHeader>Pago Pendiente</GenericHeader>
-                <div className='rejected-icon'>
-                    <FontAwesomeIcon icon={faClock} style={{color: '#ff9933'}}/>
-                </div>
-                <div className='rejected-payment-body'>
-                    <h3 className='rejected-payment-body-title-pending'>El pago esta siendo procesado</h3>
-                    <div className='rejected-payment-body-text-container'>
-                        <p className='rejected-payment-body-text'>Cuando el pago sea confirmado te enviaremos una notificación con un link para continuar con el proceso de la consulta medica.</p>
-                    </div>
-                    <Link to={'/'} className='rejected-payment-body-link' onClick={handleClick}>Ir al inicio</Link>
-                </div>
-            </div>
-        }
-        {paid === 'true' && mercadoPago &&
-            <div className='rejected-payment'>
-                <GenericHeader>Pago Confirmado</GenericHeader>
-                <div className='rejected-icon'>
-                    <img src={payedIcon} alt='payedIcon'/>
-                </div>
-                <div className='rejected-payment-body'>
-                    <h3 className='rejected-payment-body-title-confirmed'>¡Hemos recibo el pago!</h3>
-                    <div className='rejected-payment-body-text-container'>
-                        {/* <p className='rejected-payment-body-text'>Algun texto que se les ocurra poner en esta parte.</p> */}
-                        <p className='rejected-payment-body-text'>Toca en "Continuar" para seguir con el proceso de la consulta medica.</p>
-                    </div>
-                    <button className='btn btn-blue-lg mb-5' onClick={handleClick}>Continuar</button>
-                </div>
-            </div>
-        }
-        {!mercadoPago && 
-            <div className='rejected-payment'>
-                <GenericHeader>Falta de pago</GenericHeader>
-                <div className='rejected-icon'>
-                    <img src={crossIcon} alt='crossIcon'/>
-                </div>
-                <div className='rejected-payment-body'>
-                    <h3 className='rejected-payment-body-title-rejected'>Debes realizar un pago para obtener el servicio.</h3>
-                    <div className='rejected-payment-body-text-container'>
-                        <p className='rejected-payment-body-text'>El link que utilizaste ya caducó.</p>
-                    </div>
-                    <Link to={'/'} className='rejected-payment-body-link'>Ir al inicio</Link>
-                </div>
-            </div>
-        }
     </>
     )
 }
