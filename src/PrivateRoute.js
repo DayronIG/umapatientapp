365de69868769f4528df1e6bc1613fd88246b63b
santiago-rodriguezs <srodriguez@emergencias.com.ar>
2021-04-26 17:52:16 -0300

    merged well

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState, useContext, useCallback } from "react";
 import { useDispatch, useSelector } from 'react-redux'
 import { Route, withRouter, useHistory } from "react-router-dom";
-import { getAntecedents, getBenficiaries } from '../src/store/actions/firebaseQueries';
 import { AuthContext } from "./components/User/Auth";
 // ----- Login
 import LoginComponent from "./components/User/Login/Login";
@@ -23,7 +22,7 @@ const Login = () => {
         let timeout = setTimeout(() => setDelay(true), 2000)
         return () => clearTimeout(timeout)
     }, [])
-    
+
     if (delay) {
         return <LoginComponent />
     } else {
@@ -52,15 +51,15 @@ const PrivateRoute = ({ component: RouteComponent, authed, ...rest }) => {
                 subscription = queryUser.onSnapshot(async function (doc) {
                     let data = doc.data()
                     if (data?._start_date && data._start_date !== '') {
-                        dispatch({ 
-                            type: 'SET_CALL_ROOM', 
-                            payload: { 
+                        dispatch({
+                            type: 'SET_CALL_ROOM',
+                            payload: {
                                 room: data.call.room,
-                                token: data.call.token, 
+                                token: data.call.token,
                                 assignation: data.call.assignation_id,
                                 dependant: data.call.dependant,
                                 activeUid: data.call.activeUid
-                            } 
+                            }
                         })
                         if (!call.callRejected && !rest.path.includes('/attention/') && !rest.path.includes('/onlinedoctor/queue/')) {
                             setNotification(true)
@@ -82,19 +81,6 @@ const PrivateRoute = ({ component: RouteComponent, authed, ...rest }) => {
             }
         }
     }, [user, firestore, call.callRejected, rest.path])
-
-    useEffect(() => {
-        if(currentUser && !localStorage.getItem('beneficiaries')) {
-            dispatch(getBenficiaries(currentUser.uid))
-        }else if(localStorage.getItem('beneficiaries')) {
-            dispatch({ type: 'GET_BENEFICIARIES', payload: JSON.parse(localStorage.getItem('beneficiaries'))});
-        }
-        if(currentUser && !localStorage.getItem('userHistory')) {
-            dispatch(getAntecedents(currentUser.uid)) 
-        }else if(localStorage.getItem('userHistory')) {
-            dispatch({type: 'GET_HISTORY', payload: JSON.parse(localStorage.getItem('userHistory'))})
-        }
-    }, [currentUser])
 
     useEffect(() => {
         if (user.core_id) {
@@ -152,7 +138,7 @@ const PrivateRoute = ({ component: RouteComponent, authed, ...rest }) => {
 			}
 		}
 	}
-	
+
 	const handleSubmit = useCallback((device) => {
         setTimeout(() => {
             let data = {
@@ -176,7 +162,7 @@ const PrivateRoute = ({ component: RouteComponent, authed, ...rest }) => {
                 })
         }, 1500);
     }, [currentUser])
-    
+
     return (
 
         <>
