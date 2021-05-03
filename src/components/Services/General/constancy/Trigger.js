65c4dfa30c9e18d91db93f931c74fb7f425325fa
cristian <cmorales@emergencias.com.ar>
2021-05-03 13:38:49 -0300

    constancy

---
+++ src/components/Services/General/constancy/Trigger.js
@@ -0,0 +1,76 @@
+import moment from "moment"
+import { useRef } from "react"
+import { FaDownload } from "react-icons/fa"
+import ReactToPrint from "react-to-print"
+import Constancy from "./constancia"
+import UMAlogo from '../../../../assets/logo.png'
+import EMERlogo from '../../../../assets/logo-emergencias.png'
+
+const ConstancyTrigger = ({rowData, CustomButton = triggerButton}) => {
+    const ref = useRef()
+    const {patient, id, lab, service, dt_cierre} = rowData.data
+
+    const patientData = [
+        {label: 'Paciente', value: patient.fullname},
+        {label: 'DNI', value: patient.dni},
+        {label: 'Fecha', value: moment(dt_cierre ? dt_cierre.toDate() : new Date()).format("DD/MM/YYYY")},
+    ]
+    const patientDataEn = [
+        {label: 'Patient', value: patient.fullname},
+        {label: 'DNI', value: patient.dni},
+        {label: 'Date', value: moment(dt_cierre ? dt_cierre.toDate() : new Date()).format("DD/MM/YYYY")},
+    ]    
+
+    const resultsHelper = {
+        POSITIVE: 'POSITIVO',
+        NEGATIVE: 'NEGATIVO'
+    }
+
+    const methodHelper = {
+        es: {
+            'Antígeno': 'Inmuno-cronomatográco (PANIBO™ COVID-19 AG RAPID TEST DEVICE)',
+            'PCR Express': 'IRT - PCR'
+        },
+        en : {
+            'Antígeno': 'Immune-chronatographic (PANIBO™ COVID-19 AG RAPID TEST DEVICE)',
+            'PCR Express': 'IRT - PCR'
+        }
+    }
+
+    const langHelper = {
+        'PCR Express': ['es', 'en'],
+        'Antígeno': ['es']
+    }
+
+    return (
+        <>
+            <ReactToPrint 
+                trigger={() => <CustomButton /> || <FaDownload className='buttonDownload'/>}
+                content={() => ref.current}
+            />   
+            <div className="pdfWrapper" ref={ref}>
+                <Constancy
+                    id={rowData.id}
+                    result={{
+                        es: resultsHelper[lab.result_lab],
+                        en: lab.result_lab
+                    }}
+                    patientData={{
+                        es: patientData,
+                        en: patientDataEn
+                    }}
+                    logo={[UMAlogo, EMERlogo]}
+                    lang={langHelper[service]}
+                    studieType={service}
+                    method = {{
+                        es: methodHelper.es[service],
+                        en: methodHelper.en[service]
+                    }}
+                    validator= 'Dr. Fernando Luis Raffo M. N. 6.925'
+                />
+            </div>
+        </>
+    )
+}
+
+export default ConstancyTrigger;
